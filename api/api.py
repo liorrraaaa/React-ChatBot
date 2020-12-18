8dbed7c2ac927aef65a5e513d42ded5d192d9c19
liorrraaaa <lnd170130@utdallas.edu>
2020-12-17 18:15:34 -0600

    Flask and React

--- api/api.py
+++ api/api.py
@@ -1,8 +1,16 @@
+#Set virtual environment:
+#   python3 -m venv venv
+#   . venv/bin/activate
+
+#Run servers:
+#   run flask
+#   npm start
+
 import time
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
